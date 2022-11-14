be54a96a5da56cd4bc1cd038543cf21710c34f8d
Peace0902 <102469424+Peace0902@users.noreply.github.com>
2022-11-14 12:33:57 +0700

    Update Common.cpp

--- Common.cpp
+++ Common.cpp
@@ -1,4 +1,19 @@
 #include "Common.h"
+void _Common::fixConsoleWindow()
+{
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style &~(WS_MAXIMIZEBOX)&~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+void _Common::setConsoleWindow(int w, int h)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+
+	MoveWindow(console, r.left, r.top, w, h, TRUE);
+}
 void _Common::gotoXY(int x, int y)
 {
 	COORD coord;
